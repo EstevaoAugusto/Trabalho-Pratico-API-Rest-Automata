8d5af2915ca380dfc4d16771e2d36b8152cf1649
Estevão Augusto <estevaoaugusto2005@gmail.com>
2025-02-06 20:44:24 -0300

    feat: adicionar rota POST de validar MT

---
+++ automatasClasses.py
@@ -0,0 +1,31 @@
+# Turing Machine Model
+
+from pydantic import BaseModel
+from typing import Set, Tuple, Dict, List
+
+class TuringMachine(BaseModel):
+    states: Set[str]
+    input_symbols: Set[str]
+    tape_symbols: Set[str]
+    transitions: Dict[str, Dict[str, Tuple[str, str, str]]]
+    initial_state: str
+    blank_symbol: str
+    final_states: Set[str]
+    
+class PushdownAutomata(BaseModel):
+    states: Set[str]
+    input_symbols: Set[str]
+    stack_symbols: Set[str]
+    transitions: Dict[str, Dict[str, Tuple[str, str, str]]]
+    initial_state: str
+    initial_stack_symbol: str
+    final_states: Set[str]
+    acceptance_mode: str
+    
+class FiniteAutomataDeterministic(BaseModel):
+    states: Set[str]
+    input_symbols: Set[str]
+    transitions: Dict[str, Dict[str, str]]
+    initial_state: str
+    final_states: Set[str]
+    allow_partial: bool
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from typing import Union
-
 from fastapi import FastAPI
 from user import User
-from mt import TuringMachine
+
+from automata.tm.dtm import DTM # Importando maquina de turing deterministica
+from automata.tm.ntm import NTM # Importando maquina de turing nao deterministica
+from automata.pda.dpda import DPDA # Importando automata de pilha deterministica
+from automata.pda.npda import NPDA # Importando automata de pilha nao deterministica
+from automata.fa.dfa import DFA # Importando automata finito deterministico
+
+
+from automatasClasses import TuringMachine, PushdownAutomata, FiniteAutomataDeterministic
+
 
 app = FastAPI()
 
@@ -25,5 +33,71 @@ def create_user(user: User):
     return { "user_name": user.email, "user_email": user.email}
 
 @app.post("/turing/")
-def validate_turing_machine(turing: TuringMachine): # O codigo funciona, mas é preciso instalar a biblioteca do automato para poder manipula-lo diretamente no codigo
-    return {"turing": turing.states}
+def validate_turing_machine(turing: TuringMachine): 
+    dtm = DTM(
+        states = turing.states,
+        input_symbols = turing.input_symbols,
+        tape_symbols = turing.tape_symbols,
+        transitions = turing.transitions,
+        initial_state = turing.initial_state,
+        blank_symbol = turing.blank_symbol,
+        final_states = turing.final_states,
+        
+    )
+    
+    return {
+        "states": turing.states,
+        "input_symbols": turing.input_symbols,
+        "tape_symbols": turing.tape_symbols,
+        "transitions": turing.transitions,
+        "initial_state": turing.initial_state,
+        "blank_symbol": turing.blank_symbol,
+        "final_states": turing.final_states
+    }
+
+@app.post("/finite/")
+def validate_finite_automata_deterministic(finite: FiniteAutomataDeterministic): 
+    dfa = DFA(
+        states = finite.states,
+        input_symbols = finite.input_symbols,
+        tape_symbols = finite.tape_symbols,
+        transitions = finite.transitions,
+        initial_state = finite.initial_state,
+        blank_symbol = finite.blank_symbol,
+        final_states = finite.final_states,
+        
+    )
+    
+    return {
+        "states": finite.states,
+        "input_symbols": finite.input_symbols,
+        "tape_symbols": finite.tape_symbols,
+        "transitions": finite.transitions,
+        "initial_state": finite.initial_state,
+        "blank_symbol": finite.blank_symbol,
+        "final_states": finite.final_states
+    }
+    
+@app.post("/pushdown/")
+def validate_pushdown_automata(pushdown: PushdownAutomata): 
+    dpda = DPDA(
+        states = pushdown.states,
+        input_symbols = pushdown.input_symbols,
+        stack_symbols = pushdown.stack_symbols,
+        transitions = pushdown.transitions,
+        initial_state = pushdown.initial_state,
+        initial_state_symbol = pushdown.initial_stack_symbol,
+        final_states = pushdown.final_states,
+        acceptance_mode = pushdown.acceptance_mode,
+        
+    )
+    
+    return {
+        "states": pushdown.states,
+        "input_symbols": pushdown.input_symbols,
+        "stack_symbols": pushdown.stack_symbols,
+        "transitions": pushdown.transitions,
+        "initial_state": pushdown.initial_state,
+        "final_states": pushdown.final_states,
+        "allow_partial": pushdown.allow_partial,
+    }
